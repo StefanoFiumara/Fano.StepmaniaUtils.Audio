bcacb03016009b8a5d9d60dfe6aba4a7c4bf9d87
Stefano Fiumara <sdf10517@gmail.com>
2022-10-02 16:40:42 -0500

    project upload

---
+++ Fano.StepmaniaUtils.Audio.Core/StepmaniaUtilsExt.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FFmpeg.NET;
+using StepmaniaUtils;
+using StepmaniaUtils.Enums;
+
+namespace Fano.StepmaniaUtils.Audio.Core
+{
+    public static class StepmaniaUtilsExt
+    {
+        public static async Task CreatePreviewClips(string songsFolderPath)
+        {
+            // Parse .sm files asynchronously
+            var fileQueue = new BlockingCollection<string>();
+            var smFiles = new ConcurrentBag<SmFile>();
+            
+            var producer = Task.Run(() =>
+            {
+                foreach (var file in Directory.EnumerateFiles(songsFolderPath, "*.sm", SearchOption.AllDirectories).AsParallel())
+                {
+                    fileQueue.Add(file);
+                }
+
+                fileQueue.CompleteAdding();
+            });
+            
+            var consumers = Enumerable.Range(0, Environment.ProcessorCount * 2)
+                .Select(_ => Task.Run(() =>
+                {
+                    foreach (var file in fileQueue.GetConsumingEnumerable())
+                    {
+                        try
+                        {
+                            var song = new SmFile(file);
+                            smFiles.Add(song);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Error loading file at: {file}\n{e.Message}");
+                        }
+                    }
+                }));
+            
+            await Task.WhenAll(consumers);
+
+            var ffmpeg = new Engine(@"ffmpeg.exe");
+            var outputPath = Path.Combine(Directory.GetCurrentDirectory(), "preview-clip-output");
+
+            Directory.CreateDirectory(outputPath);
+
+            foreach (var smFile in smFiles)
+            {
+                var songPath = Path.Combine(songsFolderPath, smFile.Group, smFile.Directory, smFile[SmFileAttribute.MUSIC]);
+                // Console.WriteLine($"Creating Preview Clip for:{songPath}");
+
+                var inputFile = new InputFile(songPath);
+                var outputFile = new OutputFile(Path.Combine(outputPath, smFile.Group, $"{smFile.SongTitle}{inputFile.FileInfo.Extension}"));
+
+                if (!Directory.Exists(outputFile.FileInfo.Directory?.FullName))
+                {
+                    Directory.CreateDirectory(outputFile.FileInfo.Directory?.FullName);
+                }
+
+                Console.WriteLine($"Output File Path: {outputFile.FileInfo.FullName}");
+
+                var sampleStart = double.Parse(smFile[SmFileAttribute.SAMPLESTART]);
+                var sampleLength = double.Parse(smFile[SmFileAttribute.SAMPLELENGTH]);
+                
+                var options = new ConversionOptions
+                {
+                    Seek = TimeSpan.FromSeconds(sampleStart),
+                    MaxVideoDuration = TimeSpan.FromSeconds(sampleLength),
+                };
+
+                await ffmpeg.ConvertAsync(inputFile, outputFile, options, CancellationToken.None);
+            }
+            
+            
+            // TODO: Parse preview clip start and length
+            // TODO: Use FFMPEG to create new audio clip from SmFile mp3
+            // TODO: Save new preview audio clip to new folder
+        }
+    }
+}
+
---
+++ Fano.StepmaniaUtils.Audio.Tests/StepmaniaUtilsAudioTests.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading.Tasks;
+using Fano.StepmaniaUtils.Audio.Core;
+using Xunit;
+
+namespace Fano.StepmaniaUtils.Audio.Tests
+{
+    public class StepmaniaUtilsAudioTests
+    {
+        [Fact]
+        public async Task FfmpegTest()
+        {
+            await StepmaniaUtilsExt.CreatePreviewClips(@"G:\Stepmania\Songs");
+        }
+    }
+}
